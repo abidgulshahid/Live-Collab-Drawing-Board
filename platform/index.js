ecffdbf010960aff956023a939df920ecd67cc2a
Abidgul Shahid <abidgulshahid@gmail.com>
2024-12-12 22:18:01 +0500

    Update index.js

--- platform/index.js
+++ platform/index.js
@@ -6,6 +6,7 @@ const userRouter = require("./router/user_routes");
 const bodyParser = require('body-parser')
 const client = require('./Utils/redis_connection')
 const cors = require("cors");
+const uWS = require('uWebSockets.js');
 
 
 const init = async()=>{
@@ -19,6 +20,30 @@ init()
 
 // Fetching Database URL from the .env file that's available in the root location of this project.
 const mongoString = process.env.DATABASE_URL;
+
+
+// Create a uWebSocket server
+const wsApp = uWS.App();
+
+let clients = [];
+
+// WebSocket connection handling
+wsApp.ws('/*', {
+    open: (ws) => {
+        clients.push(ws);
+        ws.subscribe('draw')
+        console.log(ws)
+    },
+    message: (ws, message, isBinary) => {
+      const decodedMessage = Buffer.from(message).toString();
+      console.log(decodedMessage, 'decoded message')
+      // Broadcast the drawing data to all clients
+      ws.publish('draw', decodedMessage, isBinary);
+  },
+    close: (ws, code, message) => {
+        clients = clients.filter(client => client !== ws);
+    }
+});
 
 
 // Connecting to Database
@@ -47,7 +72,13 @@ app.get('/', (req, res) => {
 
 app.use(cors({credentials: true, origin: true}));
 
-
+wsApp.listen(3001, (token) => {
+  if (token) {
+      console.log('WebSocket server running at ws://localhost:3001', token);
+  } else {
+      console.log('Failed to start WebSocket server');
+  }
+});
 
 app.use("/api/v1/user", userRouter);
 
