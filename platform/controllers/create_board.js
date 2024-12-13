b223169e25a102b634becb0566e2c3247ff0dd08
Abidgul Shahid <abidgulshahid@gmail.com>
2024-12-14 03:02:30 +0500

    Update create_board.js

--- platform/controllers/create_board.js
+++ platform/controllers/create_board.js
@@ -1,11 +1,11 @@
-const {User, Journal}  = require("../models/user_models");
+const {User, Boards}  = require("../models/user_models");
 const jwt = require("jsonwebtoken");
 const { v4: uuidv4 } = require('uuid');
 
 
 exports.CreateBoard = async (req, res) => {
     try {
-    const { access_token } = req.body
+    const { access_token, name } = req.body
 
       const decoded = jwt.decode(access_token);
       const userId = decoded.userId
@@ -18,16 +18,27 @@ exports.CreateBoard = async (req, res) => {
 
       const randomUUID = uuidv4();
 
-  
+      const createBoards = new Boards ({
+        userID: user.id, 
+        name: name, 
+        uuid: randomUUID
+      })
+      await createBoards.save();
+
+
+      if (createBoards) 
+      {          
       return res
         .status(200)
         .json({          
             'uuid': randomUUID,
             'message': 'Login Successfully'
          });
+
+      }
     } catch (error) {
       console.log(error.message);
-      return res.status(500).json({ message: "Error during login" });
+      return res.status(500).json({ message: "Error while generating session" });
     }
   };
   
